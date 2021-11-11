ea280d83065d976ce27f5591823cd22da192edac
Ege Sari <e.sari@student.science.ru.nl>
2021-11-11 17:15:48 +0100

    Initial commit

---
+++ Uphill road/Assets/BallSpawn.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawn : MonoBehaviour
+{
+
+    [SerializeField] GameObject spherePrefab;
+    [SerializeField] Transform spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+            SpawnBall();
+    }
+
+    public void SpawnBall()
+    {
+        Instantiate(spherePrefab, spawnPoint.position, Quaternion.identity);
+    }
+}
