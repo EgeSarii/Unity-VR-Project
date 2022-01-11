2d5777fac299f33a8c46e01328c10fea88b3cda7
Ege Sari <e.sari@student.science.ru.nl>
2022-01-11 08:55:54 +0100

    Update r2d2.cs

--- Servant/Assets/Scripts/r2d2.cs
+++ Servant/Assets/Scripts/r2d2.cs
@@ -6,27 +6,50 @@ using UnityEngine.SceneManagement;
 	
 public class r2d2 : MonoBehaviour
 {
-
 	public GameObject canvas;
 	public AudioManager audio ;
     public Camera cam;
     public NavMeshAgent robot;
 
-    private Vector3 mousePos;
+    private Vector3 playerPos;
     private Vector3 destination;
 	private RectTransform rectTransform;
-	private bool mes1 = true;
-	private bool mes2 = true;
-	private bool mes3 = true;
-
 
 	string[] entranceMessagesStr = new string []{
 		"Oh you must be new! Welcome to the Game!",
 		"If you want to play just go and enter the door",
 		"We will meet there!"
 	};
+	bool[] entranceMes = new bool[] {true, true, true};
+	string[] stairsMessagesStr = new string []{
+		"Hey look at the stairs!",
+		"It seems it always go up or always down",
+		"Interesting, right?",
+		"Do you wanna inspect?",
+		"Then teleport on it!"
+	};
+	bool[] stairsMes = new bool[] {true, true, true,true,true};
 	
-	
+	string[]onStairsMessageStr = new string[]{
+		"See if the stairs ends or not",
+		"Does this end?",
+		"After your discovery get on the ground"
+	};
+
+	bool[] onStairsMes = new bool[] {true, true, true};
+
+	string[]afterStairsMessageStr = new string[]{
+		"Did you understand the illusion?",
+		"It is called Penrose stairs",
+		"Let's move to the climbing wall!"
+	};
+
+	bool[] afterStairsMes = new bool[] {true, true, true};
+
+
+
+
+
 	void Start()
 	{
 		audio =FindObjectOfType<AudioManager>();
@@ -35,18 +58,22 @@ public class r2d2 : MonoBehaviour
 	}
     void Awake ()
     {
-     	mousePos = Input.mousePosition;
+     	playerPos = Input.mousePosition;
     }
     
     // Update is called once per frame
     void Update()
     {		
-        if (mousePos != Input.mousePosition)
+        if (playerPos != Input.mousePosition)
         {	
 			MoveRobot(Input.mousePosition);	
-			mousePos = Input.mousePosition;
+			playerPos = Input.mousePosition;
 		}
-		StartCoroutine(Entrance(5));
+		StartCoroutine(Entrance(3));
+		StartCoroutine(Stairs(2));
+		StartCoroutine(OnStairs(2));
+		StartCoroutine(AfterStairs(2));
+
     }
 
 	void MoveRobot(Vector3 dest)
@@ -81,30 +108,67 @@ public class r2d2 : MonoBehaviour
 	}
 	IEnumerator Entrance(int t)
     {
-		if(Vector3.Distance(robot.transform.position, destination) <= 0.3f )
+		if(Vector3.Distance(robot.transform.position, destination) <= 0.3f )//add if it is an entrance scene
 		{ 
-			if(mes1)
+			for(int i = 0; i< entranceMessagesStr.Length; i++)
 			{
-				audio.Play("Excited");
-				ShowInfo(entranceMessagesStr[0]);
-				yield return new WaitForSecondsRealtime(t);
-				mes1 = false;
-			}
-			if(mes2)
-			{
-				audio.Play("Excited");
-				ShowInfo(entranceMessagesStr[1]);
-				yield return new WaitForSecondsRealtime(t);
-				mes2 = false;
-			}
-			if(mes3)
-			{
-				audio.Play("Excited");	
-				ShowInfo(entranceMessagesStr[2]);
-				yield return new WaitForSecondsRealtime(t);
-				mes3 = false;
+				if (entranceMes[i])
+				{
+					audio.Play("Excited");
+					ShowInfo(entranceMessagesStr[i]);
+					yield return new WaitForSecondsRealtime(t);
+					entranceMes[i]= false;
+				}
 			}
 		}	
-	}	
+	}
+
+	IEnumerator Stairs(int t)
+    {
+		for(int i = 0; i< stairsMessagesStr.Length; i++)
+		{
+			if (stairsMes[i])
+			{
+				audio.Play("Excited");
+				ShowInfo(stairsMessagesStr[i]);
+				yield return new WaitForSecondsRealtime(t);
+				stairsMes[i]= false;
+			}
+		}	
+	}
+	IEnumerator OnStairs(int t)
+    {
+		Vector3 newPos = new Vector3(0.3f, 0.3f,2.0f);
+		if(Vector3.Distance(playerPos, newPos)<=1.0f)
+		{
+			for(int i = 0; i< onStairsMessageStr.Length; i++)
+			{
+				if (onStairsMes[i])
+				{
+					audio.Play("Excited");
+					ShowInfo(onStairsMessageStr[i]);
+					yield return new WaitForSecondsRealtime(t);
+					onStairsMes[i]= false;
+				}
+			}
+		}
+	}
+	IEnumerator AfterStairs(int t)
+    {
+		
+		if(playerPos.y<=1.0f)
+		{
+			for(int i = 0; i< afterStairsMessageStr.Length; i++)
+			{
+				if (afterStairsMes[i])
+				{
+					audio.Play("Excited");
+					ShowInfo(afterStairsMessageStr[i]);
+					yield return new WaitForSecondsRealtime(t);
+					afterStairsMes[i]= false;
+				}
+			}
+		}
+	}
 	
 }
